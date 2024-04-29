b10e3b1be758d2210e2ceb086a3837c7e6bca945
alissonpeixer <alissonpeixer4@gmail.com>
2024-04-28 22:56:36 -0300

    feat: fix to session storege to local

--- src/services/token-storage.service.ts
+++ src/services/token-storage.service.ts
@@ -12,30 +12,30 @@ export class TokenStorageService {
   constructor() { }
 
   signOut(): void {
-    window.sessionStorage.clear();
+    window.localStorage.clear();
   }
 
   public saveToken(token: string): void {
-    window.sessionStorage.removeItem(this.TOKEN_KEY);
-    window.sessionStorage.setItem(this.TOKEN_KEY, token);
+    window.localStorage.removeItem(this.TOKEN_KEY);
+    window.localStorage.setItem(this.TOKEN_KEY, token);
 
   }
 
   public getToken(): string | null {
-    return window.sessionStorage.getItem(this.TOKEN_KEY);
+    return window.localStorage.getItem(this.TOKEN_KEY);
   }
 
   public saveRefreshToken(token: string): void {
-    window.sessionStorage.removeItem(this.REFRESHTOKEN_KEY);
-    window.sessionStorage.setItem(this.REFRESHTOKEN_KEY, token);
+    window.localStorage.removeItem(this.REFRESHTOKEN_KEY);
+    window.localStorage.setItem(this.REFRESHTOKEN_KEY, token);
   }
 
   public getRefreshToken(): string | null {
-    return window.sessionStorage.getItem(this.REFRESHTOKEN_KEY);
+    return window.localStorage.getItem(this.REFRESHTOKEN_KEY);
   }
 
   public clearTokens(): void {
-    sessionStorage.removeItem(this.TOKEN_KEY);
-    sessionStorage.removeItem(this.REFRESHTOKEN_KEY);
+    localStorage.removeItem(this.TOKEN_KEY);
+    localStorage.removeItem(this.REFRESHTOKEN_KEY);
   }
 }
