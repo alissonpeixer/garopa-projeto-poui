b569eb9eec742ff8c93d7c4c0c4f45c9811fd1ba
alissonpeixer <alissonpeixer4@gmail.com>
2024-04-28 22:52:36 -0300

    feat: logout button

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { PoMenuItem } from '@po-ui/ng-components';
+import { PoMenuItem, PoToolbarAction, PoDialogService } from '@po-ui/ng-components';
+
+import { Router } from '@angular/router';
+import { TokenStorageService } from '../../services/token-storage.service';
 
 @Component({
   selector: 'app-master',
@@ -8,22 +11,49 @@ import { PoMenuItem } from '@po-ui/ng-components';
 })
 export class MasterComponent implements OnInit {
 
-  constructor(){}
+
+  public readonly poMenuItem: Array<PoMenuItem>;
+  public readonly poProfileAction: Array<PoToolbarAction>;
+
+
+  constructor(
+    private router: Router,
+    private poDialogService: PoDialogService,
+    private tokenStorageService: TokenStorageService,
+  ){
+    this.poProfileAction = [
+      { label: 'Sair', icon:'po-icon-exit', type: 'danger', action: this.userLogout.bind(this) },
+    ];
+
+
+    this.poMenuItem = [
+      { label: 'Compras', icon: 'po-icon-handshake',
+        subItems: [
+          {
+            label: 'Clientes', icon: 'po-icon-user',
+            subItems: [
+              { label: 'Lista',   link:'/compras/cliente' },
+              { label: 'Incluir', link:'/compras/incluir' },
+            ]
+          },
+        ]
+      }
+    ];
+  }
 
   ngOnInit() {
   }
 
-  readonly menus: Array<PoMenuItem> = [
-    { label: 'Compras', icon: 'po-icon-handshake',
-      subItems: [
-        {
-          label: 'Clientes', icon: 'po-icon-user',
-          subItems: [
-            { label: 'Lista',   link:'/compras/cliente' },
-            { label: 'Incluir', link:'/compras/incluir' },
-          ]
-        },
-      ]
-    }
-  ];
+
+  private userLogout(): void {
+    this.poDialogService.confirm({
+      title: 'Sair',
+      message: 'Deseja relamente deslogar?',
+      confirm: ()=> {
+        this.tokenStorageService.clearTokens();
+        this.router.navigateByUrl('/login');
+      }
+    })
+  }
+
 }
