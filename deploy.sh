7f57bfa13a9c84c8936cd271575cd15f0d031b29
Alisson Peixer <48291580+alissonpeixer@users.noreply.github.com>
2025-11-27 10:48:45 -0300

    feat: atualizando deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CONTAINER_NAME="central-frontend"
+CONTAINER_NAME="garopa-projeto-poui"
+DOCKER_IMAGE_NAME="garopa:poui"
 
+docker build -t $DOCKER_IMAGE_NAME
 
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
   docker stop $CONTAINER_NAME
   docker rm $CONTAINER_NAME
 fi
 
-docker build -t central:front-end .
-
-docker run -d -p 7778:80 --name $CONTAINER_NAME central:front-end
+docker run -d -p 7778:80 --name $CONTAINER_NAME $DOCKER_IMAGE_NAME
